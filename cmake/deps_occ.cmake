125eabe320e460673c6d5c7bcea76765ff48cfb6
krande <kristoffer_andersen@outlook.com>
2025-01-18 08:56:26 +0100

    further work

--- cmake/deps_occ.cmake
+++ cmake/deps_occ.cmake
@@ -7,17 +7,11 @@ if (OpenCASCADE_FOUND)
     message(STATUS "OpenCASCADE library directory: " ${OpenCASCADE_LIBRARY_DIR})
 
     include_directories(${OpenCASCADE_INCLUDE_DIR})
-    link_directories(${OpenCASCADE_LIBRARY_DIR})
+    #link_directories(${OpenCASCADE_LIBRARY_DIR})
     # Order of linking is important
     # https://dev.opencascade.org/node/71506#comment-847
     list(APPEND
             ADA_CPP_LINK_LIBS
-   "TKBin"     "TKCAF"       "TKDEPLY"  "TKG2d"      "TKMesh"    "TKShHealing" "TKXCAF"
-   "TKBinL"    "TKCDF"       "TKDESTEP" "TKG3d"      "TKMeshVS"  "TKStd"       "TKXMesh"
-   "TKBinTObj" "TKDE"        "TKDESTL"  "TKGeomAlgo" "TKOffset"  "TKStdL"      "TKXml"
-   "TKBinXCAF" "TKDECascade" "TKDEVRML" "TKGeomBase" "TKOpenGl"  "TKTObj"      "TKXmlL"
-   "TKBO"      "TKDEGLTF"    "TKernel"  "TKHLR"      "TKPrim"    "TKTopAlgo"   "TKXmlTObj"
-   "TKBool"    "TKDEIGES"    "TKFeat"   "TKLCAF"     "TKRWMesh"  "TKV3d"       "TKXmlXCAF"
-   "TKBRep"    "TKDEOBJ"     "TKFillet" "TKMath"     "TKService" "TKVCAF"      "TKXSBase"
+            ${OpenCASCADE_LIBRARIES}
     )
 endif (OpenCASCADE_FOUND)
